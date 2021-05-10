41b445f95411aa51aa8a5336d15ec46e8341c151
vietnamican <vietnamican@gmail.com>
2021-05-10 10:02:57 +0700

    fix bug in loss function

--- models/loss.py
+++ models/loss.py
@@ -9,9 +9,11 @@ class PointLoss(nn.Module):
         self.loss = nn.MSELoss(reduction='sum')
 
     def forward(self, pred, gt):
-        mask = gt.gt(0).float()
-        loss = self.loss(pred * mask, gt)
-        loss = loss / (mask.sum() + 1e-4)
+        mask = gt.gt(0)
+        pred_ = pred[mask]
+        gt_ = pred[gt]
+        loss = self.loss(pred_, gt_)
+        loss = loss / (mask.float().sum() + 1e-4)
         return loss
 
 
@@ -22,7 +24,9 @@ class RegLoss(nn.Module):
         self.loss = nn.SmoothL1Loss(reduction='sum')
 
     def forward(self, pred, gt):
-        mask = gt.gt(0).float()
-        loss = self.loss(pred * mask, gt)
-        loss = loss / (mask.sum() + 1e-4)
+        mask = gt.gt(0)
+        pred_ = pred[mask]
+        gt_ = gt[mask]
+        loss = self.loss(pred_, gt_)
+        loss = loss / (mask.float().sum() + 1e-4)
         return loss
